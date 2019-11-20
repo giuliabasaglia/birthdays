164b5e715f8cdde1cf4d9fe85a8252c99903931a
sofiabenetti99 <sofia.benetti99@gmail.com>
2019-11-20 14:42:48 -0500

    deleted

--- sysexample.py
+++
@@ -1,8 +0,0 @@
-import sys
-if len(sys.argv)==3:
-    name= sys.argv[1]
-    data= sys.argv[2]
-else:
-    print("Please run the program like: "
-          "python3 main.py username data") 
-    exit() 
