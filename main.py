61d78eed78bc033bddfcb2d34879f8d7a5080b93
hapoli <anna.poli@student.gmail.com>
2019-12-04 12:27:20 -0500

    test di sysargv

--- main.py
+++ main.py
@@ -4,24 +4,38 @@ from folder1 import birthdays
 import sys
 import argparse
 
-#def parse_arg:
-parser = argparse.ArgumentParser(prog="This program returns the birthdays of a list of famous people")
-parser.add_argument("name", help='You have to insert a name in the format: "Name Surname"')  # c'è un modo per impostare il formato?
-#  c'è un modo per far sì che all'utente esca la lista di nomi contenuti nel dizionario?
-# vorremmo fare in modo che inserendo la data di nascita ritorni il nome?
-parser.add_argument("-v", "--verbosity",choices= [0,1,2], help='Decide the level of verbosity')
-args = parse.parse_args()
-date = birthdays.return_birthday(args.name)
-if args.verbosity == 2:
-    print("{} was born the {}".format(args.name, date)
-elif args.verbosity == 1:
-    print("{}:{}".format(args.name, date)
+def parse_arg():
+    parser = argparse.ArgumentParser(prog='This program returns the birthdays of a list of famous people')
+    parser.add_argument('name', help='You have to insert a name in the format: "Name Surname"')  
+    parser.add_argument('-v', '--verbosity',choices= [0,1,2], help='Decide the level of verbosity', type=int)
+    args = parser.parse_args()
+    complete_name = args.name
+    return args
+    
+birth = birthdays.return_birthday(complete_name)
+if birthdays.return_birthday(complete_name):
+    if args.verbosity == 2:
+        print('{} was born the {}'.format(complete_name, birth)
+    elif args.verbosity == 1:
+    	print('{}:{}'.format(complete_name, birth)
+    else:
+        print(birth)
 else:
-    print(date)
+    print('Sorry {} is not in the list'.format(complete_name)
 
+'''
+if __name__ == '__main__':
+    args = parse_arg()
+    birthdays.return_birthday(args)
+'''
 
-#if len(sys.argv)>1:
-#    birthdays.return_birthday(sys.argv[1])
-#else:
-#    print("You didn't pass any argument")
-
+'''
+def verbosity_level():
+    date = birthdays.return_birthday(args.name)
+    if args.verbosity == 2:
+        print("{} was born the {}".format(args.name, date)
+    elif args.verbosity == 1:
+        print("{}:{}".format(args.name, date)
+    else:
+        print(date)
+'''
