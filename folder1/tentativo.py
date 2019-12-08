ce7f6bf3c5dce87fa9c8e57e8965930bfc319407
hapoli <anna.poli@student.gmail.com>
2019-12-08 18:08:31 +0100

    action count

--- folder1/tentativo.py
+++ folder1/tentativo.py
@@ -13,7 +13,7 @@ parser = argparse.ArgumentParser(
          prog= 'This program return the birthday of famous people')
 parser.add_argument('n', nargs='+', 
          help='You can insert one or names in the format: "Name Surname"')  
-parser.add_argument('-v', '--verbosity', type=int, choices =[0,1,2], 
+parser.add_argument('-v', '--verbosity', action='count', default=0,
          help='Decide the level of verbosity')
 
 args = parser.parse_args()
@@ -26,22 +26,22 @@ name = args.n
 # verbosity option
 
 for i in name:
-    if args.verbosity:
-        if return_birthday(i):
-            if args.verbosity == 2:
-                print('{} was born the {}'.format(i, return_birthday(i)))
-            elif args.verbosity == 1:
-                print ('{}:{}'.format(i, return_birthday(i)))
-            else:
-                print (return_birthday(i))
-        else:
-            print ('Sorry, {} is not in the list, '.format(i))
-            print_birthdays()
-    else:
-        if return_birthday(i):
+    #if args.verbosity:
+    if return_birthday(i):
+        if args.verbosity >= 2:
             print('{} was born the {}'.format(i, return_birthday(i)))
+        elif args.verbosity >= 1:
+            print ('{} : {}'.format(i, return_birthday(i)))
         else:
-            print('Sorry, {} is not in the list, '.format(i))
-            print_birthdays()
+            print (return_birthday(i))
+    else:
+        print ('Sorry, {} is not in the list, '.format(i))
+        print_birthdays()
+    #else:
+     #   if return_birthday(i):
+      #      print('{} was bormnmmmnjnjnj the {}'.format(i, return_birthday(i)))
+       # else:
+        #    print('Sorry, {} is not in the list, '.format(i))
+         #   print_birthdays()
 	    
 
